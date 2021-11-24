d74e7db81745a1b04c450ddd161d8c27369a608f
novoforce <ashish.space5@gmail.com>
2021-11-24 09:21:24 +0530

    sum of arr added

---
+++ Udemy/Recursion/11_sum_of_array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum_of_array_1(int arr[], int n){
+
+    //base case
+    if(n == 0){
+        return 0;
+    }
+    return arr[0] + sum_of_array_1(arr+1,n-1);
+}
+
+int sum_of_array_2(int arr[], int n){
+
+    //base case
+    if(n == 0){
+        return 0;
+    }
+    return arr[n-1] + sum_of_array_2(arr,n-1);
+}
+
+
+
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "The sum of arrays:> " << sum_of_array_1(arr,n) << endl;
+    cout << "The sum of arrays:> " << sum_of_array_2(arr,n) << endl;
+
+
+    return 0;
+}
